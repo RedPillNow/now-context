3fbfe5813521b20baaa8720723dfe706f7bd1004
kito99 <kito.mann@virtua.com>
2016-09-02 09:15:48 -0400

    Initial commit

---
+++ now-basic-app-layout.ts
@@ -0,0 +1,8 @@
+/// <reference path="bower_components/polymer-ts/polymer-ts.d.ts"/>
+
+@component('now-basic-app-layout')
+class BasicAppLayout extends polymer.Base {
+
+}
+
+BasicAppLayout.register();
