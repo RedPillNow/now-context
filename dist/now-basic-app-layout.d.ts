043be4c1863fbdfc9fea758f93e6ac1ee835a6d5
kito99 <kito.mann@virtua.com>
2016-09-06 20:38:50 -0400

    New build

--- dist/now-basic-app-layout.d.ts
+++ dist/now-basic-app-layout.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../polymer-ts/polymer-ts.d.ts" />
+/// <reference path="bower_components/polymer-ts/polymer-ts.d.ts" />
 declare namespace NowElements {
     class BasicAppLayout extends polymer.Base {
         prop1: string;
